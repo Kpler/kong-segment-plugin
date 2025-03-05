a30b26202526004880e7cf093905ce9247574b7e
Yann Rouillard <yann@pleiades.fr.eu.org>
2025-03-05 14:39:49 +0100

    feat: allow to configure the segment url

--- kong/plugins/segment/schema.lua
+++ kong/plugins/segment/schema.lua
@@ -1,29 +1,36 @@
-local typedefs = require "kong.db.schema.typedefs"
+local typedefs = require("kong.db.schema.typedefs")
 
 local PLUGIN_NAME = "segment"
 
 local schema = {
-    name = PLUGIN_NAME,
-    fields = { -- the 'fields' array is the top-level entry with fields defined by Kong
+  name = PLUGIN_NAME,
+  fields = { -- the 'fields' array is the top-level entry with fields defined by Kong
     {
-        consumer = typedefs.no_consumer
+      consumer = typedefs.no_consumer,
     }, -- this plugin cannot be configured on a consumer (typical for auth plugins)
     {
-        protocols = typedefs.protocols_http
-    }, {
-        config = {
-            -- The 'config' record is the custom part of the plugin schema
-            type = "record",
-            fields = { -- a standard defined field (typedef), with some customizations
-              {
-                  write_key = { -- self defined field
-                    type = "string",
-                    required = true
-                  }
-              } -- adding a constraint for the value
-            }
-        }
-    }}
+      protocols = typedefs.protocols_http,
+    },
+    {
+      config = {
+        -- The 'config' record is the custom part of the plugin schema
+        type = "record",
+        fields = { -- a standard defined field (typedef), with some customizations
+          {
+            write_key = { -- self defined field
+              type = "string",
+              required = true,
+            },
+          },
+          {
+            segment_url = typedefs.url({
+              default = "https://api.segment.io/v1/track",
+            }),
+          }, -- adding a constraint for the value
+        },
+      },
+    },
+  },
 }
 
 return schema
--- spec/segment/01-schema_spec.lua
+++ spec/segment/01-schema_spec.lua
@@ -1,31 +1,47 @@
+---@diagnostic disable: undefined-field
 local PLUGIN_NAME = "segment"
 
 -- helper function to validate data against a schema
 local validate
 do
-    local validate_entity = require("spec.helpers").validate_plugin_config_schema
-    local plugin_schema = require("kong.plugins." .. PLUGIN_NAME .. ".schema")
+  local validate_entity = require("spec.helpers").validate_plugin_config_schema
+  local plugin_schema = require("kong.plugins." .. PLUGIN_NAME .. ".schema")
 
-    function validate(data)
-        return validate_entity(data, plugin_schema)
-    end
+  function validate(data)
+    return validate_entity(data, plugin_schema)
+  end
 end
 
 describe(PLUGIN_NAME .. ": (schema)", function()
+  it("accepts a write_key string parameter", function()
+    local ok, err = validate({
+      write_key = "abcd",
+    })
+    assert.is_nil(err)
+    assert.is_truthy(ok)
+  end)
 
-    it("accepts a write_key string parameter", function()
-        local ok, err = validate({
-            write_key = "abcd"
-        })
-        assert.is_nil(err)
-        assert.is_truthy(ok)
-    end)
+  it("requires a write_key string parameter", function()
+    local ok, err = validate({})
+    assert.is_same({ config = { write_key = "required field missing" } }, err)
+    assert.is_nil(ok)
+  end)
 
-    it("requires a write_key string parameter", function()
-      local ok, err = validate({
-      })
-      assert.is_same({config = { write_key = "required field missing"}}, err)
-      assert.is_nil(ok)
-    end)
+  it("accepts a segment_url string parameter", function()
+    local ok, err = validate({
+      write_key = "abcd",
+      segment_url = "https://api.segment.io/v1/track",
+    })
+    assert.is_nil(err)
+    assert.is_truthy(ok)
+  end)
 
+  it("reject an invalid segment_url parameter", function()
+    local ok, err = validate({
+      write_key = "abcd",
+      segment_url = "not-a-url",
+    })
+    assert.is_same({ config = { segment_url = "missing host in url" } }, err)
+    assert.is_nil(ok)
+  end)
 end)
